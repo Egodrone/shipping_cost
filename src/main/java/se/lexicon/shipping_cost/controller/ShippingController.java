3dcf985689d586b527db2bb7126a951dd0880fd7
Eddie <eddie.ch.89d@gmail.com>
2021-04-12 09:42:43 +0200

    setups

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -2,10 +2,25 @@ package se.lexicon.shipping_cost.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.PostConstruct;
 
 
 @Controller
 public class ShippingController {
+
+
+    @PostConstruct
+    public void init() {
+
+    }
+
+
+    @GetMapping("/AddBoxForm")
+    public String index() {
+        return "AddBoxForm";
+    }
 
 
 }
