67999f2bbbca90cc3a65581e2981a85e53203e0a
Eddie <eddie.ch.89d@gmail.com>
2021-04-12 09:52:02 +0200

    Box Repository class

--- src/main/java/se/lexicon/shipping_cost/repository/BoxRepository.java
+++ src/main/java/se/lexicon/shipping_cost/repository/BoxRepository.java
@@ -1,4 +1,12 @@
 package se.lexicon.shipping_cost.repository;
 
-public interface BoxRepository {
+import se.lexicon.shipping_cost.entity.Box;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface BoxRepository extends CrudRepository<Box, String> {
+
+    List<Box> findAll();
 }
