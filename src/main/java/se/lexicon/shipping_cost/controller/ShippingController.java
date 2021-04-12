9f46e0d7eec981ba4c5ed29dfdd2a51710ec7dd9
Eddie <eddie.ch.89d@gmail.com>
2021-04-12 19:59:18 +0200

    Details form

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -1,13 +1,16 @@
 package se.lexicon.shipping_cost.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.lexicon.shipping_cost.entity.Box;
+import se.lexicon.shipping_cost.repository.BoxRepository;
 
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
@@ -15,11 +18,18 @@ import javax.validation.Valid;
 
 @Controller
 public class ShippingController {
+    BoxRepository boxRepository;
 
 
     @PostConstruct
     public void init() {
 
+    }
+
+
+    @Autowired
+    public ShippingController(BoxRepository boxRepository) {
+        this.boxRepository = boxRepository;
     }
 
 
@@ -30,7 +40,10 @@ public class ShippingController {
 
 
     @GetMapping("/AddBoxForm")
-    public String add() {
+    public String add(Model model) {
+        Box box = new Box();
+        model.addAttribute("box", box);
+
         return "AddBoxForm";
     }
 
@@ -44,6 +57,8 @@ public class ShippingController {
     @PostMapping("/AddBoxForm")
     public String addBox(@ModelAttribute("box") Box box, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         System.out.println("Create new box ");
+        System.out.println(box.getCountry());
+        System.out.println(box.getName());
 
         //if statements here
         //validation etc..
@@ -52,6 +67,7 @@ public class ShippingController {
             return "AddBoxForm";
         } else {
             //save to the db
+            //boxRepository.save();
 
             return "redirect:/ShowBoxList/";
         }
