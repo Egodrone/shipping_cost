78ff6e1aa2f85c0e2be3fd0fcfadeff6b52b35ce
Eddie <eddie.ch.89d@gmail.com>
2021-04-12 10:46:33 +0200

    controller etc..

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -2,9 +2,15 @@ package se.lexicon.shipping_cost.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import se.lexicon.shipping_cost.entity.Box;
 
 import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 
 
 @Controller
@@ -30,10 +36,26 @@ public class ShippingController {
 
 
     @GetMapping("/ShowBoxList")
-    public String index_test() {
+    public String box_list() {
         return "ShowBoxList";
     }
 
+
+    @PostMapping("/AddBoxForm")
+    public String addBox(@ModelAttribute("box") Box box, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        System.out.println("Create new box ");
+
+        //if statements here
+        //validation etc..
+        if (bindingResult.hasErrors()) {
+
+            return "AddBoxForm";
+        } else {
+            //save to the db
+
+            return "redirect:/ShowBoxList/";
+        }
+    }
 
 
 }
